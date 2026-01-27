ead0d95be0615efe402adabe0ad14fc58c8336df
Peter Gagliardi <ptrgags@gmail.com>
2026-01-27 17:20:44 +0000

    Redo how the signals are combined

--- src/sketches/BooleanColorSketch.ts
+++ src/sketches/BooleanColorSketch.ts
@@ -40,10 +40,53 @@ function modify(modifier: DigitalSignal, button: DigitalSignal): DigitalSignal {
   })
 }
 
-function no_modifiers(button: DigitalSignal, modifiers: DigitalSignal[]): DigitalSignal {
-  return new ObserverSignal(() => {
-    return button.value && modifiers.every((x) => !x.value)
-  })
+interface ButtonLayout {
+  a_button: DigitalSignal
+  b_button: DigitalSignal
+  x_button: DigitalSignal
+  y_button: DigitalSignal
+  up_button: DigitalSignal
+  down_button: DigitalSignal
+}
+
+interface VirtualCounter {
+  increment: DigitalSignal
+  decrement: DigitalSignal
+}
+
+type Modifier = 'a_button' | 'b_button' | 'x_button' | 'y_button'
+
+/**
+ * Make a signal that's +1 when a counter should increment, -1 when a counter
+ * should decrement, and
+ * @param modifier_id Which button to use as the modifier for gamepad/keyboard
+ * @param gamepad Gamepad buttons
+ * @param keyboard Keyboard buttons
+ * @param virtual_counter Pair of virtual buttons for pointer input
+ * @returns A signal representing
+ */
+function make_delta_signal(
+  modifier_id: Modifier,
+  gamepad: ButtonLayout,
+  keyboard: ButtonLayout,
+  virtual_counter: VirtualCounter
+): TwoButtonAxis {
+  const decrement = new ReleaseSignal(
+    new DigitalCascade([
+      modify(gamepad[modifier_id], gamepad.down_button),
+      modify(keyboard[modifier_id], keyboard.down_button),
+      virtual_counter.decrement
+    ])
+  )
+
+  const increment = new ReleaseSignal(
+    new DigitalCascade([
+      modify(gamepad[modifier_id], gamepad.up_button),
+      modify(keyboard[modifier_id], keyboard.up_button),
+      virtual_counter.increment
+    ])
+  )
+  return new TwoButtonAxis(decrement, increment)
 }
 
 export class BooleanColorSketch implements QuadMachineSketch {
@@ -57,133 +100,117 @@ export class BooleanColorSketch implements QuadMachineSketch {
   // start with an interesting operator, AND
   // See constants in the shader
   boolean_op: number = 1
-  bit_count: number = 3
+  bit_depth: number = 3
 
   // these pulse 1.0 when the uniform should be incremented (mod n)
   // and -1.0 when the uniform should be decremented
   palette_a_delta: AnalogSignal = new AnalogConst(0.0)
   palette_b_delta: AnalogSignal = new AnalogConst(0.0)
-  op_delta: AnalogSignal = new AnalogConst(0.0)
-  bit_delta: AnalogSignal = new AnalogConst(0.0)
+  operator_delta: AnalogSignal = new AnalogConst(0.0)
+  bit_depth_delta: AnalogSignal = new AnalogConst(0.0)
 
   configure_input(input: InputSystem) {
-    // keyboard input
-    const key_z = input.keyboard.digital_key('KeyZ')
-    const key_x = input.keyboard.digital_key('KeyX')
-    const key_a = input.keyboard.digital_key('KeyA')
-    const arrow_up = input.keyboard.digital_key('ArrowUp')
-    const arrow_down = input.keyboard.digital_key('ArrowDown')
+    const keyboard_buttons: ButtonLayout = {
+      a_button: input.keyboard.digital_key('KeyZ'),
+      b_button: input.keyboard.digital_key('KeyX'),
+      x_button: input.keyboard.digital_key('KeyA'),
+      y_button: input.keyboard.digital_key('KeyS'),
+      up_button: input.keyboard.digital_key('ArrowUp'),
+      down_button: input.keyboard.digital_key('ArrowDown')
+    }
 
-    // gamepad input
-    const gamepad_a = input.gamepad.digital_button(GamepadButtons.A)
-    const gamepad_b = input.gamepad.digital_button(GamepadButtons.B)
-    const gamepad_y = input.gamepad.digital_button(GamepadButtons.Y)
-    const dpad_up = input.gamepad.digital_button(GamepadButtons.Up)
-    const dpad_down = input.gamepad.digital_button(GamepadButtons.Down)
+    const gamepad_buttons: ButtonLayout = {
+      a_button: input.gamepad.digital_button(GamepadButtons.A),
+      b_button: input.gamepad.digital_button(GamepadButtons.B),
+      x_button: input.gamepad.digital_button(GamepadButtons.B),
+      y_button: input.gamepad.digital_button(GamepadButtons.Y),
+      up_button: input.gamepad.digital_button(GamepadButtons.Up),
+      down_button: input.gamepad.digital_button(GamepadButtons.Down)
+    }
 
-    // pointer input
+    // Pointer input works differently. There's a pair of increment/decrement
+    // buttons for each uniform
+    const vb_palette_a = {
+      decrement: input.pointer.virtual_button(
+        new Vec2(0, GRID_Y + SQUARE_SIZE.y),
+        new Vec2(SQUARE_SIZE.x, 8 * SQUARE_SIZE.y)
+      ),
+      increment: input.pointer.virtual_button(
+        new Vec2(0, GRID_Y + 9 * SQUARE_SIZE.y),
+        new Vec2(SQUARE_SIZE.x, 8 * SQUARE_SIZE.y)
+      )
+    }
+    const vb_palette_b = {
+      decrement: input.pointer.virtual_button(
+        new Vec2(SQUARE_SIZE.x, GRID_Y),
+        new Vec2(8 * SQUARE_SIZE.x, SQUARE_SIZE.y)
+      ),
+      increment: input.pointer.virtual_button(
+        new Vec2(9 * SQUARE_SIZE.x, GRID_Y),
+        new Vec2(8 * SQUARE_SIZE.x, SQUARE_SIZE.y)
+      )
+    }
+    const vb_operator = {
+      decrement: input.pointer.virtual_button(
+        new Vec2(150 * PIXEL.x, 0),
+        new Vec2(100 * PIXEL.x, 100 * PIXEL.y)
+      ),
+      increment: input.pointer.virtual_button(
+        new Vec2(250 * PIXEL.x, 0),
+        new Vec2(100 * PIXEL.x, 100 * PIXEL.y)
+      )
+    }
+    const vb_bit_depth = {
+      decrement: input.pointer.virtual_button(
+        new Vec2(0, 400 * PIXEL.y),
+        new Vec2(250 * PIXEL.x, 100 * PIXEL.y)
+      ),
+      increment: input.pointer.virtual_button(
+        new Vec2(250 * PIXEL.x, 400 * PIXEL.y),
+        new Vec2(250 * PIXEL.x, 100 * PIXEL.y)
+      )
+    }
 
-    const vb_palette_a_decrement = input.pointer.virtual_button(
-      new Vec2(0, GRID_Y + SQUARE_SIZE.y),
-      new Vec2(SQUARE_SIZE.x, 8 * SQUARE_SIZE.y)
+    this.palette_a_delta = make_delta_signal(
+      'a_button',
+      gamepad_buttons,
+      keyboard_buttons,
+      vb_palette_a
     )
-    const vb_palette_a_increment = input.pointer.virtual_button(
-      new Vec2(0, GRID_Y + 9 * SQUARE_SIZE.y),
-      new Vec2(SQUARE_SIZE.x, 8 * SQUARE_SIZE.y)
+    this.palette_b_delta = make_delta_signal(
+      'b_button',
+      gamepad_buttons,
+      keyboard_buttons,
+      vb_palette_b
     )
-    const vb_palette_b_decrement = input.pointer.virtual_button(
-      new Vec2(SQUARE_SIZE.x, GRID_Y),
-      new Vec2(8 * SQUARE_SIZE.x, SQUARE_SIZE.y)
+    this.operator_delta = make_delta_signal(
+      'x_button',
+      gamepad_buttons,
+      keyboard_buttons,
+      vb_operator
     )
-    const vb_palette_b_increment = input.pointer.virtual_button(
-      new Vec2(9 * SQUARE_SIZE.x, GRID_Y),
-      new Vec2(8 * SQUARE_SIZE.x, SQUARE_SIZE.y)
+    this.bit_depth_delta = make_delta_signal(
+      'y_button',
+      gamepad_buttons,
+      keyboard_buttons,
+      vb_bit_depth
     )
-    const vb_op_decrement = input.pointer.virtual_button(
-      new Vec2(150 * PIXEL.x, 0),
-      new Vec2(100 * PIXEL.x, 100 * PIXEL.y)
-    )
-    const vb_op_increment = input.pointer.virtual_button(
-      new Vec2(250 * PIXEL.x, 0),
-      new Vec2(100 * PIXEL.x, 100 * PIXEL.y)
-    )
-
-    // Merge the inputs, with priority Gamepad > Keyboard > Pointer
-    const palette_a_increment = new ReleaseSignal(
-      new DigitalCascade([
-        modify(gamepad_a, dpad_up),
-        modify(key_z, arrow_up),
-        vb_palette_a_increment
-      ])
-    )
-    const palette_a_decrement = new ReleaseSignal(
-      new DigitalCascade([
-        modify(gamepad_a, dpad_down),
-        modify(key_z, arrow_down),
-        vb_palette_a_decrement
-      ])
-    )
-    const palette_b_increment = new ReleaseSignal(
-      new DigitalCascade([
-        modify(gamepad_b, dpad_up),
-        modify(key_x, arrow_up),
-        vb_palette_b_increment
-      ])
-    )
-    const palette_b_decrement = new ReleaseSignal(
-      new DigitalCascade([
-        modify(gamepad_b, dpad_down),
-        modify(key_x, arrow_down),
-        vb_palette_b_decrement
-      ])
-    )
-    const bit_increment = new ReleaseSignal(
-      new DigitalCascade([modify(gamepad_y, dpad_up), modify(key_a, arrow_up)])
-    )
-    const bit_decrement = new ReleaseSignal(
-      new DigitalCascade([modify(gamepad_y, dpad_down), modify(key_a, arrow_down)])
-    )
-
-    const all_keyboard_modifiers = [key_z, key_x, key_a]
-    const all_gamepad_modifiers = [gamepad_a, gamepad_b, gamepad_y]
-    const op_increment = new ReleaseSignal(
-      new DigitalCascade([
-        no_modifiers(dpad_up, all_gamepad_modifiers),
-        no_modifiers(arrow_up, all_keyboard_modifiers),
-        vb_op_increment
-      ])
-    )
-    const op_decrement = new ReleaseSignal(
-      new DigitalCascade([
-        no_modifiers(dpad_down, all_gamepad_modifiers),
-        no_modifiers(arrow_down, all_keyboard_modifiers),
-        vb_op_decrement
-      ])
-    )
-
-    // finally, let's combine the increment/decrement pairs
-    // into a digital axis so the value is either -1.0 or 1.0
-    // when the button is released
-    this.palette_a_delta = new TwoButtonAxis(palette_a_decrement, palette_a_increment)
-    this.palette_b_delta = new TwoButtonAxis(palette_b_decrement, palette_b_increment)
-    this.op_delta = new TwoButtonAxis(op_decrement, op_increment)
-    this.bit_delta = new TwoButtonAxis(bit_decrement, bit_increment)
 
     // Create signals for the uniforms
     const palette_a = new ObserverSignal(() => this.palette_a)
     const palette_b = new ObserverSignal(() => this.palette_b)
     const boolean_op = new ObserverSignal(() => this.boolean_op)
-    const bit_count = new ObserverSignal(() => this.bit_count)
+    const bit_depth = new ObserverSignal(() => this.bit_depth)
     input.configure_uniforms({
-      analog: [palette_a, palette_b, boolean_op, bit_count]
+      analog: [palette_a, palette_b, boolean_op, bit_depth]
     })
   }
 
   update(time: number) {
     this.palette_a_delta.update(time)
     this.palette_b_delta.update(time)
-    this.op_delta.update(time)
-    this.bit_delta.update(time)
+    this.operator_delta.update(time)
+    this.bit_depth_delta.update(time)
 
     this.palette_a += this.palette_a_delta.value
     this.palette_a = mod(this.palette_a, PALETTE_COUNT)
@@ -191,10 +218,10 @@ export class BooleanColorSketch implements QuadMachineSketch {
     this.palette_b += this.palette_b_delta.value
     this.palette_b = mod(this.palette_b, PALETTE_COUNT)
 
-    this.boolean_op += this.op_delta.value
+    this.boolean_op += this.operator_delta.value
     this.boolean_op = mod(this.boolean_op, BOOLEAN_COUNT)
 
-    this.bit_count += this.bit_delta.value
-    this.bit_count = mod(this.bit_count, MAX_BITS)
+    this.bit_depth += this.bit_depth_delta.value
+    this.bit_depth = mod(this.bit_depth, MAX_BITS)
   }
 }
