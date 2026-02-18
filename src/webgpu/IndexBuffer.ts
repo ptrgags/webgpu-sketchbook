368de949bb63a2adb4bcf3f306558029a53e386e
Peter Gagliardi <ptrgags@gmail.com>
2026-02-18 22:36:21 +0000

    Add an IndexBuffer type, similar to VertexBuffer but for vertices

---
+++ src/webgpu/IndexBuffer.ts
@@ -0,0 +1,39 @@
+import { SIZE_U32 } from '@/core/sizes.js'
+
+export class IndexBuffer {
+  readonly label: string
+  readonly indices: number[]
+  private buffer?: GPUBuffer
+
+  constructor(label: string, indices: number[]) {
+    this.label = label
+    this.indices = indices
+  }
+
+  create(device: GPUDevice) {
+    if (this.buffer) {
+      throw new Error('index buffer already exists!')
+    }
+
+    const index_buffer = device.createBuffer({
+      size: this.indices.length * SIZE_U32,
+      usage: GPUBufferUsage.INDEX,
+      mappedAtCreation: true
+    })
+
+    const values = new Uint32Array(index_buffer.getMappedRange())
+    values.set(this.indices)
+
+    index_buffer.unmap()
+
+    this.buffer = index_buffer
+  }
+
+  attach(pass: GPURenderPassEncoder) {
+    if (!this.buffer) {
+      throw new Error('setting index buffer before creation!')
+    }
+
+    pass.setIndexBuffer(this.buffer, 'uint32')
+  }
+}
