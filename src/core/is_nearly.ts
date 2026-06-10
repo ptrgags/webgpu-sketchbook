4b258a456ce8bc21596bc73b5f6a14a17807c858
Peter Gagliardi <ptrgags@gmail.com>
2026-06-10 14:41:07 +0000

    flesh out is_nearly

--- src/core/is_nearly.ts
+++ src/core/is_nearly.ts
@@ -1,3 +1,13 @@
-export function is_nearly(a: number, b: number): boolean {
-  throw new Error('not implemented: is_nearly')
+const DEFAULT_EPSILON = 1e-8
+
+/**
+ * Float approximately equal test.
+ * @param x First value
+ * @param y Second value
+ * @param [epsilon=DEFAULT_EPSILON] Floating point epsilon
+ * @returns True if the float values are equal up to epsilon
+ */
+export function is_nearly(x: number, y: number, epsilon: number = DEFAULT_EPSILON): boolean {
+  // TODO: If this isn't good enough, see https://github.com/ptrgags/math-notebook/blob/main/mobius/src/nearly.rs
+  return Math.abs(x - y) < epsilon
 }
