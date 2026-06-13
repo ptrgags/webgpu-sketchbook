94e5d8c2bcd26812c988549b56e720757d554fcb
Peter Gagliardi <ptrgags@gmail.com>
2026-06-13 23:34:07 +0000

    sketch out functions for Even/Odd

--- src/pga3d/Even.ts
+++ src/pga3d/Even.ts
@@ -1,1 +1,65 @@
-export class Even {}
+import type { Odd } from './Odd'
+
+export class Even {
+  constructor() {}
+  add(other: Even): Even {
+    throw new Error('not implemented')
+  }
+
+  sub(other: Even): Even {
+    throw new Error('not implemented')
+  }
+
+  dual(): Odd {
+    throw new Error('not implemented')
+  }
+
+  reverse(): Even {}
+
+  // TODO: is this the right signature?
+  vee_even(other: Even): Odd {
+    throw new Error('not implemented')
+  }
+
+  vee_odd(other: Odd): Even {
+    throw new Error('not implemented')
+  }
+
+  vee(other: Odd): Even
+  vee(other: Even): Odd
+  vee(other: Odd | Even): Odd | Even {
+    throw new Error('not implemented')
+  }
+
+  equals(other: Even): boolean {
+    throw new Error('not implemented')
+  }
+
+  sandwich_even(other: Even): Even {
+    throw new Error('not implemented')
+  }
+  sandwich_odd(other: Odd): Odd {
+    throw new Error('not implemented')
+  }
+
+  sandwich(other: Even): Even
+  sandwich(other: Odd): Odd
+  sandwich(other: Even | Odd): Even | Odd {
+    if (other instanceof Even) {
+      return this.sandwich_even(other)
+    }
+
+    return this.sandwich_odd(other)
+  }
+
+  static lerp(a: Even, b: Even, t: number): Even {
+    throw new Error('not implemented')
+  }
+
+  toString(): string {
+    throw new Error('not implemented')
+  }
+
+  static readonly ZERO = new Even(0, 0, 0, 0, 0, 0, 0, 0)
+  static readonly IDENTITY = new Even(1, 0, 0, 0, 0, 0, 0, 0)
+}
--- src/pga3d/Odd.ts
+++ src/pga3d/Odd.ts
@@ -1,3 +1,5 @@
+import type { Even } from './Even'
+
 export class Odd {
   x: number
   y: number
@@ -27,4 +29,70 @@ export class Odd {
     this.xzo = xzo
     this.yzo = yzo
   }
+
+  add(other: Odd): Odd {
+    throw new Error('not implemented')
+  }
+
+  norm_sqr(): number {
+    throw new Error('not implemented')
+  }
+
+  norm(): number {
+    throw new Error('not implemented')
+  }
+
+  scale(scalar: number): Odd {
+    throw new Error('not implemented')
+  }
+
+  normalize(): Odd {
+    throw new Error('not implemented')
+  }
+
+  neg(): Odd {
+    throw new Error('not implemented')
+  }
+
+  reverse(): Odd {
+    throw new Error('not implemented')
+  }
+
+  wedge_odd(other: Odd): Even {
+    throw new Error('not implemented')
+  }
+
+  wedge_even(other: Even): Odd {
+    throw new Error('not implemented')
+  }
+
+  wedge(other: Even): Odd
+  wedge(other: Odd): Even
+  wedge(other: Even | Odd): Even | Odd {
+    throw new Error('not implemented')
+  }
+
+  sandwich_even(other: Even): Even {
+    throw new Error('not implemented')
+  }
+
+  sandwich_odd(other: Odd): Even {
+    throw new Error('not implemented')
+  }
+
+  sandwich(other: Even): Even
+  sandwich(other: Odd): Odd
+  sandwich(other: Even | Odd): Even | Odd {
+    throw new Error('not implemented')
+  }
+
+  equals(other: Odd): boolean {
+    throw new Error('not implemented')
+  }
+
+  toString(): string {
+    throw new Error('not implemented')
+  }
+
+  static readonly ZERO = Object.freeze(new Odd(0, 0, 0, 0, 0, 0, 0, 0))
 }
