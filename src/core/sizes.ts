f8cc623a64f1e38e1237f03aa97ec11747abb6fc
Peter Gagliardi <ptrgags@gmail.com>
2026-06-14 20:19:55 +0000

    add sizes for more data types

--- src/core/sizes.ts
+++ src/core/sizes.ts
@@ -1,3 +1,10 @@
+// Integer types
+export const SIZE_U16 = 2
 export const SIZE_U32 = 4
 
+// Floating point types
 export const SIZE_F32 = 4
+
+// Vector types
+export const SIZE_VEC2F = 2 * SIZE_F32
+export const SIZE_VEC3F = 3 * SIZE_F32
