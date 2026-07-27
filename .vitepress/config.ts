fb6dcd77e2e90de3eddbea4e722ea26bf069087b
Peter Gagliardi <ptrgags@gmail.com>
2026-07-27 22:30:38 +0000

    Set up Vitepress project

---
+++ .vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  srcDir: "src",
+  
+  title: "WebGPU Sketchbook",
+  description: "Math Art Experiments in WebGPU"
+})
