3e52a680d6e8a64163c23aa04db62db8c5d211a9
Peter Gagliardi <ptrgags@gmail.com>
2026-08-02 23:54:31 +0000

    Remove dead code

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import NavbarHeader from '@/components/NavbarHeader.vue';
-import { RouterView } from 'vue-router'
-</script>
-
-<template>
-  <NavbarHeader />
-  <RouterView />
-</template>
