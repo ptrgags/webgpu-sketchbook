2993e1c5f605e6c4742e23d4bf0479c2572b3f0f
Peter Gagliardi <ptrgags@gmail.com>
2026-08-03 00:04:45 +0000

    Add list of just sketch names

---
+++ src/sketches/index.ts
@@ -0,0 +1,12 @@
+export const SKETCHES = [
+  'boolean-color-clock',
+  'platonic-solids',
+  'srgb-cube',
+  'scrambled-shapes',
+  'boolean-color',
+  'stripey-ring',
+  'oklch-visualizer',
+  'meltaway',
+  'sun-and-moon',
+  'eyes'
+]
