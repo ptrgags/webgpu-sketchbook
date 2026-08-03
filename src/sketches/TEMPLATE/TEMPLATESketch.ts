b07c747abdcc21c7e663c0ab803b088c9649c7fb
Peter Gagliardi <ptrgags@gmail.com>
2026-08-03 00:00:15 +0000

    Update template sketch

--- src/sketches/TEMPLATE/TEMPLATESketch.ts
+++ src/sketches/TEMPLATE/TEMPLATESketch.ts
@@ -1,9 +1,14 @@
+import type { Machine } from '@/webgpu/Engine.js'
 import { SHADER_LIBRARY } from '../../core/ShaderLibrary'
-import { QuadUVMode, type QuadMachineSketch } from '../../machines/QuadMachine'
+import { QuadMachine, QuadUVMode, type QuadMachineSketch } from '../../machines/QuadMachine'
 import SHADER from './TEMPLATE.wgsl?url'
 
 export class TEMPLATESketch implements QuadMachineSketch {
   uv_mode: QuadUVMode = QuadUVMode.Basic
   shader_url: string = SHADER
   imports = [SHADER_LIBRARY.sdf2d]
+
+  static make_machine(): Machine {
+    return new QuadMachine(new TEMPLATESketch())
+  }
 }
