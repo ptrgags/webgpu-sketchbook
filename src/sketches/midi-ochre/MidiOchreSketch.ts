be728680e3dd517d2d2c5b7e5b9cf49e5f2034ac
Peter Gagliardi <ptrgags@gmail.com>
2026-08-13 15:05:32 +0000

    Remove unused MIDI CC code

--- src/sketches/midi-ochre/MidiOchreSketch.ts
+++ src/sketches/midi-ochre/MidiOchreSketch.ts
@@ -3,8 +3,8 @@ import { SHADER_LIBRARY } from '../../core/ShaderLibrary.js'
 import { QuadMachine, QuadUVMode, type QuadMachineSketch } from '../../machines/QuadMachine.js'
 import SHADER from './midi_ochre.wgsl?url'
 import type { InputSystem } from '../../input/InputSystem.js'
-import { AnalogConst, DigitalConst } from '../../input/const_signal.js'
-import type { AnalogSignal, DigitalSignal } from '../../input/Signal.js'
+import { DigitalConst } from '../../input/const_signal.js'
+import type { DigitalSignal } from '../../input/Signal.js'
 
 const DUMMY_NOTE = new DigitalConst(false)
 
@@ -15,16 +15,12 @@ export class MidiOchreSketch implements QuadMachineSketch {
 
   notes: DigitalSignal[] = []
 
-  cc: AnalogSignal = new AnalogConst(0.0)
-
   configure_input(input: InputSystem) {
     this.notes = input.midi.all_pitch_signals()
-    this.cc = input.midi.cc_signal(21, 0)
 
     input.configure_uniforms({
       // Dummy note makes it easier to handle gaps in black keys
-      digital: [...this.notes, DUMMY_NOTE],
-      analog: [this.cc]
+      digital: [...this.notes, DUMMY_NOTE]
     })
   }
 
